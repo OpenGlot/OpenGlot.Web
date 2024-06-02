cd7b01e0a8ecce495c6a4101b1eb99847e144f14
devops-team-poc <70562150+devops-team-poc@users.noreply.github.com>
2024-06-02 22:31:38 +0530

    Update apply-kubernetes-config.sh

--- scripts/apply-kubernetes-config.sh
+++ scripts/apply-kubernetes-config.sh
@@ -1,11 +1,21 @@
-if [ -f /tmp/env_vars ]; then
-  source /tmp/env_vars
+if [ -f /tmp/latest_artifact_web/env_vars ]; then
+  source /tmp/latest_artifact_web/env_vars
 else
   echo "env_vars file not found!"
   exit 1
 fi
 
-echo $ECR_REPOSITORY_URI
-echot $ECR_REPOSITORY
-sed -i "s|{{IMAGE_NAME}}|$ECR_REPOSITORY_URI/$ECR_REPOSITORY:latest|g" "/tmp/k8s/openglotWeb-deployment.yaml"
-kubectl apply -f /k8s --namespace openglot
+# Remove the older artifcat
+rm -f /tmp/latest_artifact_web.zip
+rm -rf /tmp/latest_artifact_web
+
+# List all objects in the S3 bucket with the specified prefix
+latest_artifact=$(aws s3 ls $S3_BUCKET/$S3_ARTIFACT_PATH --recursive | sort | tail -n 1 | awk '{print $4}')
+
+# Download the latest artifact from S3
+aws s3 cp $S3_BUCKET/$latest_artifact /tmp/latest_artifact_web.zip
+
+# Unzip the latest artifact
+unzip /tmp/latest_artifact_web.zip -d /tmp/latest_artifact_web
+
+kubectl apply -f /tmp/latest_artifact_web/k8s/ --namespace openglot
