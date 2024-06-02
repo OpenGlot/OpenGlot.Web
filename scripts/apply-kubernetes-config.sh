1039018bb44c9f0b98cc1bc9f16ae371ba71c80d
devops-team-poc <70562150+devops-team-poc@users.noreply.github.com>
2024-06-02 15:01:30 +0530

    Update apply-kubernetes-config.sh

--- scripts/apply-kubernetes-config.sh
+++ scripts/apply-kubernetes-config.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
-cd "$CODEBUILD_SRC_DIR"
-# List files in the Kubernetes configuration directory
-echo #{variables.CLUSTER_NAME}
-echo "Listing files in Kubernetes configuration directory ($CODEBUILD_SRC_DIR/k8s):"
-ls
-kubectl apply -f k8s/ --namespace openglot
+# List all objects in the S3 bucket with the specified prefix
+latest_artifact=$(aws s3 ls s3://codepipeline-us-east-1-411495364561/openglot-web-pipelin/BuildArtif/ --recursive | sort | tail -n 1 | awk '{print $4}')
+
+# Download the latest artifact from S3
+aws s3 cp s3://codepipeline-us-east-1-411495364561/$latest_artifact /tmp/latest_artifact.zip
+
+# Unzip the latest artifact
+unzip /tmp/latest_artifact.zip -d /tmp/latest_artifact
+
+# Navigate to the directory containing the unpacked latest artifact
+cd /tmp/latest_artifact
