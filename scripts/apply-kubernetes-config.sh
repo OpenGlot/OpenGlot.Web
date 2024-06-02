f68b96409957096c756ccd49777c42e0c40426b2
devops-team-poc <70562150+devops-team-poc@users.noreply.github.com>
2024-06-02 22:42:05 +0530

    Update apply-kubernetes-config.sh

--- scripts/apply-kubernetes-config.sh
+++ scripts/apply-kubernetes-config.sh
@@ -5,13 +5,13 @@ else
   exit 1
 fi
 
-# List all objects in the S3 bucket with the specified prefix
-latest_artifact=$(aws s3 ls $S3_BUCKET/$S3_ARTIFACT_PATH --recursive | sort | tail -n 1 | awk '{print $4}')
+# # List all objects in the S3 bucket with the specified prefix
+# latest_artifact=$(aws s3 ls $S3_BUCKET/$S3_ARTIFACT_PATH --recursive | sort | tail -n 1 | awk '{print $4}')
 
-# Download the latest artifact from S3
-aws s3 cp $S3_BUCKET/$latest_artifact /tmp/latest_artifact_web.zip
+# # Download the latest artifact from S3
+# aws s3 cp $S3_BUCKET/$latest_artifact /tmp/latest_artifact_web.zip
 
-# Unzip the latest artifact
-unzip /tmp/latest_artifact_web.zip -d /tmp/latest_artifact_web
+# # Unzip the latest artifact
+# unzip /tmp/latest_artifact_web.zip -d /tmp/latest_artifact_web
 
 kubectl apply -f /tmp/latest_artifact_web/k8s/ --namespace openglot
