f4a3cf623416804225f3b74ae3af9e43532f5331
devops-team-poc <70562150+devops-team-poc@users.noreply.github.com>
2024-06-02 21:57:15 +0530

    Update update-kubeconfig.sh

--- scripts/update-kubeconfig.sh
+++ scripts/update-kubeconfig.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+if [ -f /tmp/env_vars ]; then
+  source /tmp/env_vars
+else
+  echo "env_vars file not found!"
+  exit 1
+fi
+
+echo $CLUSTER_NAME
+echo $AWS_REGION
 aws eks update-kubeconfig --name $CLUSTER_NAME --region $AWS_REGION
