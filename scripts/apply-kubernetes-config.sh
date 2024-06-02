aa7f9f7b04fa068cfd38df41615aeb5108345130
devops-team-poc <70562150+devops-team-poc@users.noreply.github.com>
2024-06-02 12:08:09 +0530

    Update apply-kubernetes-config.sh

--- scripts/apply-kubernetes-config.sh
+++ scripts/apply-kubernetes-config.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 ls
+echo "Current working directory: $(pwd)"
+if [ -n "$CODEBUILD_SRC_DIR" ]; then
+  echo "Listing files in CODEBUILD_SRC_DIR ($CODEBUILD_SRC_DIR):"
+  ls "$CODEBUILD_SRC_DIR"
+fi
 echo "Cluster Name: $CLUSTER_NAME"
 kubectl apply -f k8s/ --namespace openglot
