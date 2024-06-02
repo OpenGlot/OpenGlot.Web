3314c1728b8fd49b4fcf3fa7fbfdcb6a9fd9f01b
devops-team-poc <70562150+devops-team-poc@users.noreply.github.com>
2024-06-02 22:59:02 +0530

    Update update-kubeconfig.sh

--- scripts/update-kubeconfig.sh
+++ scripts/update-kubeconfig.sh
@@ -3,10 +3,10 @@ rm -f /tmp/latest_artifact_web.zip
 rm -rf /tmp/latest_artifact_web
 
 # List all objects in the S3 bucket with the specified prefix
-latest_artifact=$(aws s3 ls $S3_BUCKET/$S3_ARTIFACT_PATH --recursive | sort | tail -n 1 | awk '{print $4}')
+latest_artifact=$(aws s3 ls s3://codepipeline-us-east-1-411495364561/openglot-web-pipelin/BuildArtif/ --recursive | sort | tail -n 1 | awk '{print $4}')
 
 # Download the latest artifact from S3
-aws s3 cp $S3_BUCKET/$latest_artifact /tmp/latest_artifact_web.zip
+aws s3 cp s3://codepipeline-us-east-1-411495364561/$latest_artifact /tmp/latest_artifact_web.zip
 
 # Unzip the latest artifact
 unzip /tmp/latest_artifact_web.zip -d /tmp/latest_artifact_web
