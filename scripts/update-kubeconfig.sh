e45342d004d5754a60bf01a189674aa9e1ef0890
devops-team-poc <70562150+devops-team-poc@users.noreply.github.com>
2024-06-02 22:42:22 +0530

    Update update-kubeconfig.sh

--- scripts/update-kubeconfig.sh
+++ scripts/update-kubeconfig.sh
@@ -9,6 +9,15 @@ fi
 rm -f /tmp/latest_artifact_web.zip
 rm -rf /tmp/latest_artifact_web
 
+# List all objects in the S3 bucket with the specified prefix
+latest_artifact=$(aws s3 ls $S3_BUCKET/$S3_ARTIFACT_PATH --recursive | sort | tail -n 1 | awk '{print $4}')
+
+# Download the latest artifact from S3
+aws s3 cp $S3_BUCKET/$latest_artifact /tmp/latest_artifact_web.zip
+
+# Unzip the latest artifact
+unzip /tmp/latest_artifact_web.zip -d /tmp/latest_artifact_web
+
 echo $CLUSTER_NAME
 echo $AWS_REGION
 aws eks update-kubeconfig --name $CLUSTER_NAME --region $AWS_REGION
