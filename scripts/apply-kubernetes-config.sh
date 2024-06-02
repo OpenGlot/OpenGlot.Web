b876f8be766e4a01e3f4dac62a7aea815d77a05f
devops-team-poc <70562150+devops-team-poc@users.noreply.github.com>
2024-06-02 20:53:57 +0530

    Update apply-kubernetes-config.sh

--- scripts/apply-kubernetes-config.sh
+++ scripts/apply-kubernetes-config.sh
@@ -1,3 +1,10 @@
+if [ -f /tmp/env_vars ]; then
+  source /tmp/env_vars
+else
+  echo "env_vars file not found!"
+  exit 1
+fi
+
 echo $CLUSTER_NAME 
 echo #{CLUSTER_NAME}
 kubectl apply -f /k8s --namespace openglot
