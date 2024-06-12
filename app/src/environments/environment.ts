12c3feb54f8c9f36d88556d69f4d208a8518732b
devops-team-poc <70562150+devops-team-poc@users.noreply.github.com>
2024-06-12 13:52:06 +0530

    Update environment.ts

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     "USER_POOL_ID": 'us-east-1_sSfL2aipf',
     "CLIENT_ID": '2ilnvfris0u021205020gruug1',
     "AUTH_URI": 'openglot.auth.us-east-1.amazoncognito.com',
-    "REDIRECT_URI": 'https://localhost:4200/auth'
+    "REDIRECT_URI": 'https://beta.openglot.com/auth'
 };
