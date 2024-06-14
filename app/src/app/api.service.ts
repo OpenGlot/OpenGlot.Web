4c95bd407f3aa6b31641b755f2cefa7ad79434ef
devops-team-poc <70562150+devops-team-poc@users.noreply.github.com>
2024-06-14 15:30:22 +0530

    Update api.service.ts

--- app/src/app/api.service.ts
+++ app/src/app/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'https://api.openglot.com/api'; // Update with your API base URL
+  private apiUrl = 'https://api.openglot.com'; // Update with your API base URL
 
   constructor(private http: HttpClient) {}
 
