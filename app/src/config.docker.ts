ed1054f7ad457315a0c0ea4072b3101f7486af5b
paul totzke <7072k3@gmail.com>
2024-06-13 23:38:58 -0500

    remove port for docker config

--- app/src/config.docker.ts
+++ app/src/config.docker.ts
@@ -4,8 +4,8 @@ const config = {
   userPoolWebClientId: '1qei9084gdum632hggk6856j4f',
   oauth: {
     domain: 'openglot02.auth.us-east-1.amazoncognito.com',
-    redirectSignIn: 'http://localhost:8080/oauth2/callback',
-    redirectSignOut: 'http://localhost:8080/',
+    redirectSignIn: 'http://localhost/oauth2/callback',
+    redirectSignOut: 'http://localhost/',
     scope: ['openid', 'profile', 'email'],
   },
 };
