1751957fe9d602574f68dce936036b67e1e0634b
paul totzke <7072k3@gmail.com>
2024-06-19 10:35:53 -0500

    add beta configs

--- app/src/config.ts
+++ app/src/config.ts
@@ -4,9 +4,12 @@ switch (process.env.REACT_APP_ENV) {
   case 'docker':
     configValue = require('./config.docker').default;
     break;
-  case 'production':
+  case 'prod':
     configValue = require('./config.prod').default;
     break;
+  case 'beta':
+      configValue = require('./config.prod').default;
+      break;
   case 'development':
   case 'local':
   default:
