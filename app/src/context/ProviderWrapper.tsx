b8717e1c644ff7e24dbd93bb6932f7f48cb8f6ae
evelyn-2022 <evelyn20220101@gmail.com>
2024-06-24 09:51:59 +0100

    Update postman tests

--- app/src/context/ProviderWrapper.tsx
+++ app/src/context/ProviderWrapper.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ThemeProvider } from "./ThemeContext";
 import { AuthProvider } from "./AuthContext";
-import HealthCheck from "../components/common/HealthCheck";
+import HealthCheck from "components/common/HealthCheck";
 import Layout from "components/layout/Layout";
 
 const ProviderWrapper: React.FC = () => {
